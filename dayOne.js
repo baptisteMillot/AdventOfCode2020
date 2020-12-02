2cd59d436bf6dd300d570ddaef17ffa367429277
Baptiste <baptiste@itsfactory.fr>
2020-12-02 14:04:00 +0100

    delete for of

--- dayOne.js
+++ dayOne.js
@@ -1,17 +1,16 @@
-var fs = require('fs'); 
+var fs = require('fs');
 
 const YEAR_TO_FOUND = 2020;
 module.exports.dayOne = () => {
     fs.readFile('./inputs/dayOne.txt', "utf8", (err, data) => {
         const yearList = data.split("\n").map(year => parseInt(year));
-        
-        for (let year of yearList){
-            const correspondingYear = yearList
-            .flatMap(y => yearList.filter(ye => ye + y + year == YEAR_TO_FOUND));
-            if (correspondingYear.length) {
-                console.log(correspondingYear[0] * correspondingYear[1] * year)
-                break;
-            }
+
+        const correspondingYear = yearList
+        .flatMap(year => yearList.filter(ye => yearList
+            .find(y => ye + y + year === YEAR_TO_FOUND)));
+        console.log(correspondingYear)
+        if (correspondingYear.length) {
+            console.log(correspondingYear[0] * correspondingYear[1] * correspondingYear[2])
         }
       });
 };
